408500996b42b9ec6701cac01478204e890b431d
shennn0628 <91179867+shennn0628@users.noreply.github.com>
2022-11-29 23:01:42 +0800

    Update fpgrowth.py

--- fpgrowth/fpgrowth.py
+++ fpgrowth/fpgrowth.py
@@ -18,52 +18,57 @@ class Node:
             child.display(idx + 1)
 
 def getFromFile(name):
-    itemSetList = []
-    freq = []
+    itemSetList = {}
     with open(name, 'r') as file:
         reader = csv.reader(file)
+        tmp = []
         for line in reader:
             line = list(filter(None, line))
-            itemSetList.append(line)
-            freq.append(1)
-    return itemSetList, freq
+            tmp.append(line)
+        for item in tmp:
+            itemSetList[frozenset(item)] = 1
+    return itemSetList
 
-def constructTree(itemSetList, freq, minSup):
-    headertb = collections.defaultdict(int)
+def constructTree(itemSetList, minSup):
+    headertb = {}
     for idx, itemSet in enumerate(itemSetList):
         for item in itemSet:
-            headertb[item] += freq[idx]
-    headertb = dict((item, cnt) for item, cnt in headertb.items() if cnt >= minSup)
+            headertb[item] = headertb.get(item, 0) + itemSetList[itemSet]
+    for i in list(headertb.keys()):
+        if headertb[i] < minSup:
+            del(headertb[i])
     if (len(headertb) == 0):
         return None, None
+    freqItemSet = set(headertb.keys())
     for item in headertb:
         headertb[item] = [headertb[item], None]
     fpTree = Node('Null', 1, None)
-    for idx, itemSet in enumerate(itemSetList):
-        itemSet = [item for item in itemSet if item in headertb]
-        itemSet.sort(key = lambda item:headertb[item][0], reverse = True)
-        curr = fpTree
+    for itemSet, cnt in itemSetList.items():
+        itemx = {}
         for item in itemSet:
-            curr = updateTree(item, curr, headertb, freq[idx])
+            if item in freqItemSet:
+                itemx.update({item: headertb[item][0]})
+        if len(itemx) > 0:
+            items = [i[0] for i in sorted(itemx.items(), key = lambda p:(p[1], str(p[0])), reverse = True)]
+            updateTree(items, fpTree, headertb, cnt)
     return fpTree, headertb
 
-def updateHeadertb(item, node, headertb):
-    if headertb[item][1] == None:
-        headertb[item][1] = node
-    else:
-        curr = headertb[item][1]
-        while curr.nxt != None:
-            curr = curr.nxt
-        curr.nxt = node
+def updateHeadertb(item, node):
+    while item.nxt != None:
+        item = item.nxt
+    item.nxt = node
 
-def updateTree(item, node, headertb, freq):
-    if item in node.child:
-        node.child[item].increment(freq)
+def updateTree(items, node, headertb, freq):
+    if items[0] in node.child:
+        node.child[items[0]].increment(freq)
     else:
-        newnode = Node(item, freq, node)
-        node.child[item] = newnode
-        updateHeadertb(item, newnode, headertb)
-    return node.child[item]
+        node.child[items[0]] = Node(items[0], freq, node)
+        if headertb[items[0]][1] == None:
+            headertb[items[0]][1] = node.child[items[0]]
+        else:
+            updateHeadertb(headertb[items[0]][1], node.child[items[0]])
+    if len(items) > 1:
+        updateTree(items[1::], node.child[items[0]], headertb, freq)
 
 def pull(node, prefix):
     if node.fa != None:
@@ -72,74 +77,70 @@ def pull(node, prefix):
 
 def findprefix(base, headertb):
     node = headertb[base][1]
-    res = []
+    res = {}
     freq = []
     while node != None:
         prefix = []
         pull(node, prefix)
-        if len(prefix) > 1:
-            res.append(prefix[1:])
+        if len(prefix) >= 1:
+            res.update({frozenset(prefix[1:]): node.freq})
             freq.append(node.freq)
         node = node.nxt
     return res, freq
 
-def mine(headertb, minSup, prefix, freqItemList):
-    sortedItemList = [item[0] for item in sorted(list(headertb.items()), key = lambda p : p[1][0])] # sort by frequency
+def mine(headertb, minSup, prefix, freqItemList, mp):
+    sortedItemList = [item[0] for item in sorted(headertb.items(), key = lambda p : str(p[1]))] # sort by frequency
     for item in sortedItemList:
         newfreq = prefix.copy()
         newfreq.add(item)
-        freqItemList.append(newfreq)
-        [conditionalBase, freq] = findprefix(item, headertb)
-        [conditionalTree, newheadertb] = constructTree(conditionalBase, freq, minSup)
-        if newheadertb != None:
-            mine(newheadertb, minSup, newfreq, freqItemList)
+        if len(newfreq) <= 5:
+            newfreq = set(sorted(list(newfreq)))
+            freqItemList.append(newfreq)
+            [conditionalBase, freq] = findprefix(item, headertb)
+            sum = 0
+            for i in conditionalBase.values():
+                sum += i
+            mp.update({frozenset(newfreq): sum})
+            [conditionalTree, newheadertb] = constructTree(conditionalBase, minSup)
+            if newheadertb != None:
+                mine(newheadertb, minSup, newfreq, freqItemList, mp)
 
-def powerset(s):
-    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(1, len(s)))
-
-def support(testSet, itemSetList):
+def associationRule(freqItems, mp, minConf):
     cnt = 0
-    for itemSet in itemSetList:
-        if (set(testSet).issubset(itemSet)):
-            cnt += 1
-    return cnt
-
-def associationRule(freqItemSet, itemSetList, minConf):
-    rules = []
-    for itemSet in freqItemSet:
-        subsets = powerset(itemSet)
-        supp = support(itemSet, itemSetList)
-        for s in subsets:
-            conf = float(supp / support(s, itemSetList))
+    for freqItems in mp:
+        subsets = [i for n in range(1, len(freqItems)) for i in itertools.combinations(freqItems, n)]
+        for subset in subsets:
+            conf = mp[frozenset(freqItems)] / mp[frozenset(subset)]
             if conf >= minConf:
-                rules.append([set(s), set(itemSet.difference(s)), conf])
-    return rules
+                cnt += 1
+    return cnt
 
 def getFreqfromList(itemSetList):
     freq = [1 for i in range(len(itemSetList))]
     return freq
 
 def fpgrowth(name, minSup, minConf):
-    itemSetList, freq = getFromFile(name)
+    itemSetList = getFromFile(name)
     minSup = len(itemSetList) * minSup
-    fpTree, headertb = constructTree(itemSetList, freq, minSup)
+    fpTree, headertb = constructTree(itemSetList, minSup)
     if fpTree == None:
         print("No frequent item set!")
     else:
         freqitems = []
-        mine(headertb, minSup, set(), freqitems)
-        rules = associationRule(freqitems, itemSetList, minConf)
+        mp = {}
+        mine(headertb, minSup, set(), freqitems, mp)
+        rules = associationRule(freqitems, mp, minConf)
         return freqitems, rules
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option('-f', '--inputFile', dest = 'inputFile', help = 'CSV filename', default = None)
-    parser.add_option('-s', '--minSupport', dest = 'minSup', help = 'Min support (float)', default = 0.5, type = 'float')
-    parser.add_option('-c', '--minConfidence', dest = 'minConf', help = 'Min confidence (float)', default = 0.5, type = 'float')
-    [test, args] = parser.parse_args()
-    freqitems, rules = fpgrowth(test.inputFile, test.minSup, test.minConf)
-    # freqitems, rules = fpgrowth("test.csv", 0.5, 0.5)
-    # freqitems, rules = fpgrowth("mushroom.csv", 0.1, 0.8)
+    # parser = optparse.OptionParser()
+    # parser.add_option('-f', '--inputFile', dest = 'inputFile', help = 'CSV filename', default = None)
+    # parser.add_option('-s', '--minSupport', dest = 'minSup', help = 'Min support (float)', default = 0.5, type = 'float')
+    # parser.add_option('-c', '--minConfidence', dest = 'minConf', help = 'Min confidence (float)', default = 0.5, type = 'float')
+    # [test, args] = parser.parse_args()
+    # freqitems, rules = fpgrowth(test.inputFile, test.minSup, test.minConf)
+    # freqitems, rules = fpgrowth("/content/drive/MyDrive/Colab Notebooks/test.csv", 0.5, 0.5)
+    freqitems, rules = fpgrowth("/content/drive/MyDrive/Colab Notebooks/mushroom.csv", 0.1, 0.8)
     # print(freqitems)
     cnt = [0, 0, 0, 0, 0]
     for i in freqitems:
@@ -155,22 +156,12 @@ if __name__ == "__main__":
             cnt[4] += 1
     for i in cnt:
         print(i)
-    print(len(rules))
-    # for i in rules:
-    #     print(i)
+    print(rules)
 
-# [a, b] = getFromFile("test.csv")
-# [c, d] = constructTree(a, b, 0.5)
-# c.display()
-# for i in d:
-#     print(i)
-#     for j in d[i]:
-#         print(j)
-
-'''
-f,a,c,d,g,i,m,p
-a,b,c,f,l,m,o
-b,f,h,j,o
-b,c,k,s,p
-a,f,c,e,l,p,m,n
-'''
+# '''
+# f,a,c,d,g,i,m,p
+# a,b,c,f,l,m,o
+# b,f,h,j,o
+# b,c,k,s,p
+# a,f,c,e,l,p,m,n
+# '''
